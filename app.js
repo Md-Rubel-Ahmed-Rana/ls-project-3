2b87f503edb2872a1a59e689f4fdc0ae6e66b1c7
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-09 00:57:34 +0600

    contacts displayed on UI

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 const getInputValue = (id) => {
     const inputField = document.getElementById(id);
     const inputValue = inputField.value;
+    inputField.value = "";
     return inputValue
 }
 
 let myContacts = []
 
 const saveContact = () => {
+    const contactList = document.getElementById("contact-list");
+    const li = document.createElement("li");
     const name = getInputValue("name");
     const email = getInputValue("email");
     const number = getInputValue("number");
     const contact = {name, email, number};
+    const nameHead = document.createElement("h3");
+    nameHead.innerText = name;
+    const spanEmail = document.createElement("p");
+    spanEmail.innerText = email;
+    const spanNumber = document.createElement("p");
+    spanNumber.innerText = number;
+    li.appendChild(nameHead);
+    li.appendChild(spanEmail);
+    li.appendChild(spanNumber);
+    contactList.appendChild(li);
     myContacts.push(contact)
     localStorage.setItem("myContacts", JSON.stringify(myContacts));
 }
