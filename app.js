89771d9c067f659e89b7100cfd6499bbafabbdc0
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-09 01:59:19 +0600

    Contac project completed successfully

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const getInputValue = (id) => {
 }
 
 let myContacts = []
-
-const saveContact = () => {
+const displayContactsUI = () => {
     const contactList = document.getElementById("contact-list");
     const li = document.createElement("li");
     const name = getInputValue("name");
     const email = getInputValue("email");
     const number = getInputValue("number");
-    const contact = {name, email, number};
+    const contact = { name, email, number };
     const nameHead = document.createElement("h3");
     nameHead.innerText = name;
     const spanEmail = document.createElement("p");
@@ -26,5 +25,41 @@ const saveContact = () => {
     li.appendChild(spanNumber);
     contactList.appendChild(li);
     myContacts.push(contact)
-    localStorage.setItem("myContacts", JSON.stringify(myContacts));
+
+    return contactList
 }
+
+const saveContact = () => {
+    const inputField = document.getElementById("name");
+    if (inputField.value === "") {
+        return
+    }
+    displayContactsUI();
+    localStorage.setItem("myContacts", JSON.stringify(myContacts));
+};
+
+const getSavedContact = () => {
+    const savedContacts = localStorage.getItem("myContacts");
+    const contactList = document.getElementById("contact-list");
+    if (savedContacts) {
+        myContacts = JSON.parse(savedContacts);
+        myContacts.forEach(contact => {
+            const li = document.createElement("li");
+            const nameHead = document.createElement("h3")
+            nameHead.innerText = contact.name;
+
+            const spanEmail = document.createElement("p");
+            spanEmail.innerText = contact.email;
+
+            const spanNumber = document.createElement("p");
+            spanNumber.innerText = contact.number;
+
+            li.appendChild(nameHead)
+            li.appendChild(spanEmail)
+            li.appendChild(spanNumber);
+
+            contactList.appendChild(li);
+        });
+    }
+}
+getSavedContact();
